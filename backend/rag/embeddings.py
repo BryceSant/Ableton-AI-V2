edea993472d9a2af0c62a9da3669ffb6fd3204e5
BryceSant <161985646+BryceSant@users.noreply.github.com>
2025-10-05 13:57:48 -0700

    added a comment

--- backend/rag/embeddings.py
+++ backend/rag/embeddings.py
@@ -1,6 +1,6 @@
 from langchain_ollama import OllamaEmbeddings
 
-EMBEDDING_MODEL = 'nomic-embed-text:latest'
+EMBEDDING_MODEL = 'nomic-embed-text:latest' #EMBEDDING MODEL
 
 def return_embedding():
     return OllamaEmbeddings(model = EMBEDDING_MODEL)
