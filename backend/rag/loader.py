7d7e89f3f298623256bd4e2a9cb7371a78372a69
BryceSant <161985646+BryceSant@users.noreply.github.com>
2025-10-06 16:42:41 -0700

    should have completed loader.py

--- backend/rag/loader.py
+++ backend/rag/loader.py
@@ -1,5 +1,7 @@
 from pypdf import PdfReader
+from langchain.schema import Document
 import os 
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 def load_pdfs(pdf_folder_path):
     #Load empty list for pdf's
@@ -8,11 +10,43 @@ def load_pdfs(pdf_folder_path):
 
     #get the path to pdf's
     for pdf in os.listdir(pdf_folder_path):
-        print(pdf)
-        if pdf.endswith(".pdf"):
-            reader = PdfReader(os.path.join(pdf_folder_path, pdf))
-            num_pages = len(reader.pages)
-            print(num_pages)
+        print(pdf) 
+        if pdf.lower().endswith(".pdf"):
+            pdf_reader = PdfReader(os.path.join(pdf_folder_path, pdf)) #loads pdf individually
+            pdf_len = len(pdf_reader.pages) #gives number of pages
+            #pdf_meta = pdf_reader.metadata #gives possible metadata
+
+            for x in range(pdf_len):
+                page = pdf_reader.pages[x]
+                page_text = page.extract_text()
+                #print(page_text)
+
+                page_metadata = {
+                    "source": pdf,
+                    "page_number": x + 1,
+                    "file_path": pdf_folder_path,
+                }
+
+                #print(page_metadata)
+
+                pdf_list.append(Document(page_content = page_text, metadata = page_metadata))
+                #print(pdf_list)
+    
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 300, 
+        chunk_overlap = 30,
+    )
+
+    split_docs = splitter.split_documents(pdf_list)
+
+    print(split_docs)
+    
+    return split_docs
+
+
+
+
+
         
 
 
