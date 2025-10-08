a3528ead040490d363c2e8b9d53bed0b8bb69421
BryceSant <161985646+BryceSant@users.noreply.github.com>
2025-10-08 16:03:56 -0700

    added CORS in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://127.0.0.1:8000",
+    "http://localhose:8000",
+]
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
-def root():
-    return {"Hello": "World"}
+async def root():
+    return {"Hello": "Heehee"}
+
