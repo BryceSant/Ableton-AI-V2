fb40f65f126c4b2ee4a378fe3cbeb1552a3934a7
BryceSant <161985646+BryceSant@users.noreply.github.com>
2025-10-22 15:33:50 -0700

    added a temp div that shows the bot is typing, alongside proper formatting for output with pre

--- frontend/script.js
+++ frontend/script.js
@@ -8,6 +8,10 @@ async function send() {
   chatbox.innerHTML += `<div class="user"><b>You:</b> ${message}</div>`;
   msgInput.value = "";
 
+  //Temp message to be shown while it renders
+  const tempText = '<div class="bot" id="tempBot">Assistant is typing...</div>'
+  chatbox.innerHTML += tempText
+
   // Send to backend
   const res = await fetch("http://127.0.0.1:8000/chat", {
     method: "POST",
@@ -17,6 +21,19 @@ async function send() {
 
   // Display chatbot message
   const data = await res.json();
-  chatbox.innerHTML += `<div class="bot"><b>Bot:</b> ${data.reply}</div>`;
+
+  //Delete temp
+  const tempDel = document.getElementById('tempBot')
+  if (tempDel) {
+    tempDel.remove()
+  }
+  //
+
+  //Add chatbot message to chatbot
+  chatbox.innerHTML += `
+  <div class="bot">
+    <b>Bot:</b>
+    <pre style="display:inline; white-space:pre-wrap;">${data.reply}</pre>
+  </div>`;
   chatbox.scrollTop = chatbox.scrollHeight;
 }
