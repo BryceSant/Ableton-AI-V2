ae7bf32255f9652dc9e45bb8bbda0ae7d56bc5ad
Bryce Santiago <161985646+BryceSant@users.noreply.github.com>
2025-10-27 01:23:18 -0700

    added new script that allows enter key to send input to send function

---
+++ frontend/enter-button.js
@@ -0,0 +1,10 @@
+var input = document.getElementById("msg");
+
+input.addEventListener("keypress", function(event) {
+
+    if (event.key == "Enter") {
+        event.preventDefault();
+        document.getElementById("sendButton").click();
+    }
+});
+
